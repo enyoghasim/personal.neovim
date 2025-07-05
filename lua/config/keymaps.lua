df69c062ec2ca740f6ba0daafe7b9bde027653ae
David Enyoghasim <73113145+enyoghasim@users.noreply.github.com>
2025-07-05 18:56:42 +0100

    added telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,8 @@
-vim.keymap.set('n', '<leader>ff', builtin.find_files,
+vim.keymap.set('n', '<leader>ff', "<cmd>Telescope find_files<CR>",
                {desc = 'Telescope find files'})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep,
+vim.keymap.set('n', '<leader>fg', "<cmd>Telescope live_grep<CR>",
                {desc = 'Telescope live grep'})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope buffers'})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags,
+vim.keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<CR>",
+               {desc = 'Telescope buffers'})
+vim.keymap.set('n', '<leader>fh', "<cmd>Telescope help_tags<CR>",
                {desc = 'Telescope help tags'})
