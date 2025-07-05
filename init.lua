ebb2074ba0cff3906f1224991b4213cef6a785c6
David Enyoghasim <73113145+enyoghasim@users.noreply.github.com>
2025-07-05 18:08:29 +0100

    added lazyvim

--- init.lua
+++ init.lua
@@ -1,1 +1,20 @@
-print("hello from init.lua")
+-- tries getting lazy.nvim from the standard data path of Neovim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+
+-- If lazy.nvim is not found, clone it from the official repository
+-- This ensures that lazy.nvim is available for plugin management
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+print("lazy.nvim loaded from: " .. lazypath)
