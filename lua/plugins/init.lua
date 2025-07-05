abf2ff1a13a17abf6f10c33fbb83a4d0968a0d26
David Enyoghasim <73113145+enyoghasim@users.noreply.github.com>
2025-07-05 19:25:50 +0100

    nvim treesitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,5 +12,31 @@ return {
         tag = '0.1.8',
         dependencies = {'nvim-lua/plenary.nvim'},
         config = function() require("telescope").setup() end
+    }, {
+        {
+            "nvim-treesitter/nvim-treesitter",
+            branch = 'master',
+            lazy = false,
+            build = ":TSUpdate",
+            config = function()
+                require("nvim-treesitter.configs").setup({
+                    ensure_installed = {
+                        "lua", "python", "javascript", "typescript", "html",
+                        "css", "bash", "json", "yaml", "markdown",
+                        "markdown_inline", "vim", "vimdoc", "query", "regex",
+                        "go", "rust", "java", "php", "ruby", "swift", "kotlin",
+                        "dockerfile", "sql", "xml", "toml", "gitcommit"
+                    },
+                    highlight = {
+                        enable = true, -- Enable syntax highlighting
+                        additional_vim_regex_highlighting = false
+                    },
+                    indent = {
+                        enable = true -- Enable indentation
+                    }
+                })
+
+            end
+        }
     }
 }
